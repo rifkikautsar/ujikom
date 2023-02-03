b9141b23413ce4dbc99ea696074af24713e1d876
rifkikautsar <rifkikautsar1@gmail.com>
2023-02-03 07:03:00 +0700

    add error handling query

--- delete.php
+++ delete.php
@@ -18,29 +18,30 @@ if($db->connect_errno==0){
         }
     } else if(isset($_POST['id_penerbit'])){
         $id_penerbit = mysqli_real_escape_string($db, $_POST["id_penerbit"]);
-        $sql = "DELETE from penerbit where id_penerbit = '$id_penerbit'";
-        $res = $db->query($sql);
-            if($res){
-                if($db->affected_rows>0){
-                    $message="OK";
-                }else{
-                    $message="ERROR".(DEVELOPMENT?" : ".$db->error:"");
-                }
-            }else {
-                $message = "ERROR".(DEVELOPMENT?" : ".$db->error:"");
-            }
-        } else if(isset($_POST['id_kategori'])){
-            $id_kategori = mysqli_real_escape_string($db, $_POST["id_kategori"]);
-            $sql = "DELETE from kategori where id_kategori = '$id_kategori'";
+        try{
+            $sql = "DELETE from penerbit where id_penerbit = '$id_penerbit'";
             $res = $db->query($sql);
                 if($res){
                     if($db->affected_rows>0){
                         $message="OK";
-                    }else{
-                        $message="ERROR".(DEVELOPMENT?" : ".$db->error:"");
                     }
-                }else {
-                    $message = "ERROR".(DEVELOPMENT?" : ".$db->error:"");
                 }
+        }catch(Exception $e){
+            $message="ERROR ".(DEVELOPMENT?" : ".$db->error:"Tidak bisa dihapus, terdapat data buku yang berelasi dengan penerbit");
+        }
+
+        } else if(isset($_POST['id_kategori'])){
+            $id_kategori = mysqli_real_escape_string($db, $_POST["id_kategori"]);
+            try{
+                $sql = "DELETE from kategori where id_kategori = '$id_kategori'";
+                $res = $db->query($sql);
+                    if($res){
+                        if($db->affected_rows>0){
+                            $message="OK";
+                        }
+                    }
+            }catch(Exception $e){
+                $message="ERROR ".(DEVELOPMENT?" : ".$db->error:"Tidak bisa dihapus, terdapat data buku yang berelasi dengan kategori");
             }
+        }
 }echo $message;
